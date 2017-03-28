3894420ef2e4eb47a75373c7329a7a453a5344bf
zholloway <zholloway92@gmail.com>
2017-03-28 14:20:29 -0400

    first commit

---
+++ HumanRobotPanda/Being.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanRobotPanda
+{
+    class Being
+    {
+        public string Name { get; set; }
+
+        public bool IsAsleep { get; set; }
+
+        public Being()
+        {
+        }
+
+        public virtual void DisplayName()
+        {
+            Console.WriteLine(this.Name);
+        }
+
+        public virtual void DisplayGreeting()
+        {
+        }
+    }
+}
